4f3f0d22a225bdcf4040919ae381f39aa23e0840
animaligia <qualidadesintechtica@animaeducacao.com.br>
2026-08-10 14:07:04 -0300

    BIV2

--- js/config.js
+++ js/config.js
@@ -1,7 +1,14 @@
 window.BI_CONFIG = {
-  SUPABASE_URL: "https://xgtzyinfinjehxzojafn.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_4iYSQaPGuSxaaZUkwdF_lw_IueIdhRw",
+  SUPABASE_URL: "https://nkjmgzyjjbepebzurowy.supabase.co",
+
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_6Tuuyz6nYSBa782OGDi1rg_XkBHmO8U",
+
   VIEW_NAME: "vw_validacao_bi_final",
+
   ESTEIRA_PADRAO: "Jun a Ago/26 (Regular)",
-  DOMINIOS_PERMITIDOS: ["animaeducacao.com.br", "ulife.com.br"]
+
+  DOMINIOS_PERMITIDOS: [
+    "animaeducacao.com.br",
+    "ulife.com.br"
+  ]
 };
