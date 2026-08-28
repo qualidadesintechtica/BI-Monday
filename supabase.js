89adc02c5509fe4655f761f9d546008455078914
qualidadesintechtica <qualidadesintechtica@animaeducacao.com.br>
2026-08-28 11:06:30 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  "use strict";
-  if (!window.supabase) throw new Error("Supabase não carregado.");
-  if (!window.BI_CONFIG) throw new Error("BI_CONFIG não carregado.");
-  window.biSupabase = window.supabase.createClient(
-    window.BI_CONFIG.SUPABASE_URL,
-    window.BI_CONFIG.SUPABASE_PUBLISHABLE_KEY
-  );
-})();
