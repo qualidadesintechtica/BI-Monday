fdf6419d9a8fb336be66373e3f28937b1ac52cfd
Qualidade Sintetchica <qualidadesintechtica@animaeducacao.com.br>
2026-09-04 11:36:02 -0300

    V22

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.BI_CONFIG = {
   SUPABASE_URL: "https://nkjmgzyjjbepebzurowy.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_6Tuuyz6nYSBa782OGDi1rg_XkBHmO8U",
   VIEW_NAME: "vw_materiais_bi_consolidada",
-  BUILD_ID: "20260904-v21-2-sync-real",
+  BUILD_ID: "20260904-v22-amostra-padroes",
   REUNIAO_RESUMO_VIEW_NAME: "vw_nq_reuniao_resumo",
   REUNIAO_NC_VIEW_NAME: "vw_nq_reuniao_nao_conformidades",
   RESULTADOS_VIEW_NAME: "vw_resultados_alcancados",
