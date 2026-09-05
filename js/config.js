bf83ddcb7a1c36107431d7f1ac21a616ad2abc1c
Qualidade Sintetchica <qualidadesintechtica@animaeducacao.com.br>
2026-09-05 18:25:51 -0300

    V23.1

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.BI_CONFIG = {
   SUPABASE_URL: "https://nkjmgzyjjbepebzurowy.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_6Tuuyz6nYSBa782OGDi1rg_XkBHmO8U",
   VIEW_NAME: "vw_materiais_bi_consolidada",
-  BUILD_ID: "20260905-v23-importador-projetos",
+  BUILD_ID: "20260905-v23-1-menu-projetos",
   REUNIAO_RESUMO_VIEW_NAME: "vw_nq_reuniao_resumo",
   REUNIAO_NC_VIEW_NAME: "vw_nq_reuniao_nao_conformidades",
   RESULTADOS_VIEW_NAME: "vw_resultados_alcancados",
